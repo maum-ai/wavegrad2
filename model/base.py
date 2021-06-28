2360484c894cfb781b17f903d9aaf58d26554ca5
jun_mac <jun3518@mindslab.ai>
2021-06-28 09:25:51 +0900

    f

---
+++ model/base.py
@@ -0,0 +1,12 @@
+#This code is form
+#https://github.com/ivanvovk/WaveGrad
+import torch
+
+
+class BaseModule(torch.nn.Module):
+    def __init__(self):
+        super(BaseModule, self).__init__()
+    
+    @property
+    def nparams(self):
+        return sum(p.numel() for p in self.parameters() if p.requires_grad)
---
+++ model/layers.py
@@ -0,0 +1,15 @@
+#This code is form
+#https://github.com/ivanvovk/WaveGrad
+import torch
+
+from model.base import BaseModule
+
+
+class Conv1dWithInitialization(BaseModule):
+    def __init__(self, **kwargs):
+        super(Conv1dWithInitialization, self).__init__()
+        self.conv1d = torch.nn.Conv1d(**kwargs)
+        torch.nn.init.orthogonal_(self.conv1d.weight.data, gain=1)
+
+    def forward(self, x):
+        return self.conv1d(x)
